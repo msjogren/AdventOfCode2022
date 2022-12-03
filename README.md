a9039206ce4c99536b2dfa90195fd044018f4a49
Mattias Sjögren <msj@trackman.com>
2022-12-03 12:47:14 +0100

    Solution for day 1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AdventOfCode2022
-C# solutions to Advent of Code 2022
+C# solutions to Advent of Code 2022. Uses .NET 7 and C# 11.
---
+++ day01/Program.cs
@@ -0,0 +1,12 @@
+﻿var elfCalories = File.ReadAllText("input.txt")
+                      .Split("\r\n\r\n")
+                      .Select(elf => 
+                        elf.Split("\r\n")
+                           .Sum(int.Parse)
+                      );
+
+// Part 1
+Console.WriteLine(elfCalories.Max());
+
+// Part 2
+Console.WriteLine(elfCalories.OrderByDescending(c => c).Take(3).Sum());
