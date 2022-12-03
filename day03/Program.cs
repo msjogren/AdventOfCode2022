25c0a6641a0b59697faf4435fcbbc4599b2930e5
Mattias Sjögren <msj@trackman.com>
2022-12-03 12:52:10 +0100

    Solution for day 3

---
+++ day03/Program.cs
@@ -0,0 +1,25 @@
+﻿int Priority(char c) => Char.IsUpper(c) ? c - 'A' + 27 : c - 'a' + 1;
+
+var input = File.ReadAllLines("input.txt");
+
+// Part 1
+var sumOfPriorities = input
+                        .Select(line => (left:  line[..(line.Length/2)],
+                                         right: line[(line.Length/2)..]))
+                        .Sum(rucksack =>
+                            Priority(rucksack.left
+                                        .Intersect(rucksack.right)
+                                        .Single())
+                        );
+Console.WriteLine(sumOfPriorities);
+
+// Part 2
+var groupSum = input
+                .Chunk(3)
+                .Sum(group =>
+                    Priority(group[0]
+                                .Intersect(group[1])
+                                .Intersect(group[2])
+                                .Single())
+                );
+Console.WriteLine(groupSum);
